0feac69cf110723016f8c97b65029d2ef755c6cf
dartmidder <dartmidder@dec2a252-8a97-a28e-c821-f0892bc11fcb>
2011-02-10 18:51:53 +0000

    Añadiendo a trunk: cursor, data, game, intro, button, basicmenu, keyboard, mainmenu, resource, state. También comentando código de los módulos anteriores

---
+++ engine/intro.py
@@ -0,0 +1,105 @@
+#-*- encoding: utf-8 -*-
+
+import state
+import data
+import resource
+import mainmenu
+import pygame
+import xml.dom.minidom
+
+class Intro(state.State):
+    '''
+    @brief Clase encargada de representar la introducción del juego
+    consiste en un simple fadein y fadeout del logo.
+    '''
+    def __init__(self, game, xml_file):
+        '''
+        @brief Constructor 
+        
+        @param game Referencia a game
+        @param xml_file ruta del archivo xml con la configuración deseada
+        '''
+        state.State.__init__(self, game)
+        
+        pygame.display.set_caption("Zycars")
+
+        #Parseamos el fichero xml
+        parser = xml.dom.minidom.parse(data.get_path_xml(xml_file))
+        
+        #Obtenemos la velocidad con la que avanzará la introducción
+        self.speed = int(parser.firstChild.getAttribute('speed'))
+        self.actual_alpha = 0
+        
+        #Obtenemos una superficie negra del mismo tamaño que la pantalla
+        self.surface = pygame.Surface((pygame.display.get_surface().get_width(), pygame.display.get_surface().get_height()))
+        
+        #Obtenemos la imagen del logo
+        for element in parser.getElementsByTagName('image'):
+            
+            image_code = str(element.getAttribute('imagecode'))
+            x = int(element.getAttribute('x'))
+            y = int(element.getAttribute('y'))
+            
+            image = resource.get_image(image_code)
+            
+            #La dibujamos sobre la superficie antes mencionada
+            self.surface.blit(image, (x,y))
+        
+        #Indicamos el alfa de la superficie, en un principio será 0, es decir
+        #Negra completamente
+        self.surface.set_alpha(self.actual_alpha)
+        
+        #Definimos los estados en los que se encuentra la intro
+        self.to_opaque = True
+        self.quit = False
+        
+    def update(self):
+        '''
+        @brief Método que actualiza la intro
+        '''
+        #Si vamos de negro a transparente
+        if self.to_opaque:
+            
+            #Si pulsamos alguna tecla cancelamos al intro
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    self.quit = True
+            
+            #Vamos aumentado el alpha segun la velocidad indicada
+            self.actual_alpha += self.speed
+            self.surface.set_alpha(self.actual_alpha)
+            
+            #Si la superficie es completamente transparente cambiamos de estado
+            if self.actual_alpha >= 255:
+                self.to_opaque = False
+        
+        #Si vamos de transparente a negro
+        else:
+            
+            #si pulsamos alguna tecla cancelamos la intro
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    self.quit = True
+            
+            #Vamos disminuyendo el canal alpha de la superficie
+            self.actual_alpha -= self.speed
+            self.surface.set_alpha(self.actual_alpha)
+            
+            #Cuando llegamos de nuevo al negro completo, indicamos que la intro ha terminado
+            if self.actual_alpha <= 0:
+                self.quit = True
+    
+        #Si la intro se ha terminado o se ha cancelado
+        if self.quit:
+            print "Al menú principal"
+            #Pasamos al menu principal
+            self.game.change_state(mainmenu.MainMenu(self, 'menu/mainmenu.xml'))
+            
+    def draw(self, screen):
+        '''
+        @brief Método que dibuja la superficie en pantalla
+        
+        @param screen Superficie destino
+        '''
+        screen.blit(self.surface, (0,0))
+        
---
+++ engine/mainmenu.py
@@ -0,0 +1,83 @@
+#-*- encoding: utf-8 -*-
+
+import state
+import data
+import resource
+import button
+import keyboard
+import pygame
+import cursor
+import basicmenu
+import xml.dom.minidom
+import unicodedata
+
+class MainMenu(basicmenu.BasicMenu):
+    '''
+    @brief Clase que modela el comportamiento del menú principal del juego
+    '''
+    def __init__(self, game, path_xml):
+        '''
+        @brief Constructor
+        
+        @param game Referencia a game
+        @param path_xml Ruta del archivo xml con las características del menú
+        '''
+        basicmenu.BasicMenu.__init__(self, game)
+        
+        #Cambiamos el titulo de la ventana
+        pygame.display.set_caption("Zycars: Menú Principal")
+        
+        parse = xml.dom.minidom.parse(data.get_path_xml(path_xml))
+        
+        #Le pasamos el archivo parseado a BasicMenu para que obtenga los elementos básicos
+        self.parser_basic_info(parse)
+
+    def draw(self, screen):
+        '''
+        @brief Método que dibuja todos los elementos en pantalla
+        
+        @param screen Superficie destino
+        '''
+        #Dibujamos todos los elementos basicos
+        self.draw_basic_elements(screen)
+        
+        #Dibujamos el cursor
+        self.cursor.draw(screen)
+    
+    def update(self):
+        '''
+        @brief Método que actualiza los elementos del menú
+        '''
+        #Comprobamos si el punto esta situado sobre algun botón
+        self.actual_option = None
+        for button in self.buttons:
+            button.update()
+            if button.get_selected():
+                self.actual_option = button.get_option()
+        
+        #Si es asi cambiamos la imagen del cursor
+        if self.actual_option:
+            self.cursor.over()
+            if pygame.mouse.get_pressed()[0]:
+                self.treat_option()
+        else:
+            self.cursor.normal()
+        
+        #Actualizamos el cursor
+        self.cursor.update()
+            
+    def treat_option(self):
+        '''
+        @brief Método que controla la opción elegida y que hacer según el caso.
+        '''
+        if self.actual_option == u"Carrera Rápida":
+            print "Elegido: Carrera Rapida"
+        elif self.actual_option == "Campeonato":
+            print "Elegido: Campeonato"
+        elif self.actual_option == "Contrarreloj":
+            print "Ha elegido: Contrarreloj"
+        elif self.actual_option == "Opciones":
+            print "Ha elegido: Opciones"
+        elif self.actual_option == "Salir":
+            print "Ha elegido: Salir"
+            keyboard.set_quit(True)
