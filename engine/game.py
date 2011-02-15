e6c0db2db19c5bd8f058ab0260289c3a7e381c1a
dartmidder <dartmidder@dec2a252-8a97-a28e-c821-f0892bc11fcb>
2011-02-15 12:36:28 +0000

    Añadiendo cuenta atrás para las carreras

--- engine/game.py
+++ engine/game.py
@@ -1,101 +1,77 @@
 # -*- encoding: utf-8 -*-
 
 import pygame
-from pygame.color import *
 import data
 import resource
 import keyboard
-import button
-import intro
+import countdown
 import xml.dom.minidom
 
 from pygame.locals import *
 
 class Game:
     '''
-    @brief Clase encargada de inicializar pygame. Tiene el bucle principal. También lleva el control de cada estado del juego.
+    Clase encargada de inicializar pygame. Tiene el bucle principal .
+    También lleva el control de cada estado del juego.
     '''
     def __init__(self):
         '''
-        #brief Constructor. Carga e inicializa la configuración principal y las variables principales de la clase
+        Carga e inicializa la configuración principal y las variables principales de la clase
         '''
-        #Cargamos el archivo de configuración
         parser = xml.dom.minidom.parse(data.get_path_xml('configuration.xml'))
         
-        #Obtenemos las dimensiones de la pantalla
         for element in parser.getElementsByTagName('screen'):
             self.__screen_width = int(element.getAttribute('width'))
             self.__screen_height = int(element.getAttribute('height'))
             self.caption = element.getAttribute('caption')
         
-        #Obtenemos los fps
         for element in parser.getElementsByTagName('fps'):
             self.fps = int(element.getAttribute('value'))
         
-        #Inicializamos Pygame
         pygame.init()
-        
-        #Obtenemos la ventana de juego
         self.screen = pygame.display.set_mode((self.__screen_width, self.__screen_height))
         
-        #Obtenemos el icono
         for element in parser.getElementsByTagName('icon'):
             icon_code = str(element.getAttribute('code'))
             self.icon = resource.get_image(icon_code)
         
-        #Deshabilitamos el cursor
-        pygame.mouse.set_visible(False)
         pygame.display.set_icon(self.icon)
-        
-        #Creamos el reloj
         self.clock = pygame.time.Clock()
-        
-        #Estado actual del juego
-        self.__actual_state = intro.Intro(self, 'intro.xml')  
-                        
+        self.__actual_state = countdown.CountDown('cheesebu', 300, 0.002, 0.025, 5)
+            
     def run(self):
         '''
-        @brief Función que contiene el bucle principal del juego. Actualiza y dibuja el estado actual.
+        Función que contiene el bucle principal del juego.
+        Actualiza y dibuja el estado actual.
         '''
-        
-        #Mientras no cerremos la pantalla
         while not keyboard.quit():
             
             self.clock.tick(self.fps)
             
-            #Actualizamos el teclado
             keyboard.update()
             
-            #Ponemos la pantalla a negro completamente
-            self.screen.fill(THECOLORS['black'])
+            self.screen.fill((255,255,255))
             
-            #Actualizamos y dibujamos el estado actual
-            self.__actual_state.update()
-            self.__actual_state.draw(self.screen)
+            if not self.__actual_state.complete():
+                self.__actual_state.update()
+                self.__actual_state.draw(self.screen)
             
-            #Actualizamos la pantalla
             pygame.display.flip()
         
     def get_screen_width(self):
         '''
-        @brief Método que devuelve el ancho de la pantalla en píxeles.
-        
-        @return Ancho de la pantalla en píxeles.
+        Devuelve el ancho de la pantalla en píxeles
         '''
         return self.__screen_width
         
     def get_screen_height(self):
         '''
-        @brief Método que devuelve el alto de la pantalla en píxeles.
-        
-        @return Alto de la pantalla en píxeles.
+        Devuelve el alto de la pantalla en píxeles
         '''
         return self.__screen_height
         
     def change_state(self, new_state):
         '''
-        @brief Método que Recibe y cambia el nuevo estado del juego.
-        
-        @param new_state, nuevo estado del juego
+        Recibe y cambia el nuevo estado del juego.
         '''
         self.__actual_state = new_state
