58a2fa45d5c9087287e080e1d66ad5d054e9fd34
dartmidder <dartmidder@dec2a252-8a97-a28e-c821-f0892bc11fcb>
2011-02-19 17:42:07 +0000

    Cambios en Start(Línea de meta), quitando superficie auxiliar y dibujando tantas imagenes como sean necesarias, añadiendo nueva imagen para la meta también

--- engine/start.py
+++ engine/start.py
@@ -28,8 +28,10 @@ class Start:
         @param orientation Indica si la linea será horizontal o vertical
         @param car_angle angulo para situar al coche
         '''
+        #Referencias
         self.game_control = game_control
         self.circuit = circuit
+        
         #Posición
         self.x = x
         self.y = y
@@ -37,20 +39,19 @@ class Start:
         #Imagen
         self.image = resource.get_image(image_code)
         
-        print orientation
-        print car_angle
+        self.orientation = orientation
 
         #Si hemos indicado que la posición es vertical
         if orientation == 'vertical':
+            #Obtenemos una superfice con el ancho de la "carretera" del circuito como referencia
+            '''self.surface = pygame.Surface((self.image.get_width(), self.image.get_height() * self.circuit.get_circuit_width()))
+            self.surface.set_alpha(0)
+            aux_y = 0'''
             
-            #Obtenemos una superfice con el ancho de la "carretera" del circuito
-            self.surface = pygame.Surface((self.image.get_width(), self.image.get_height() * self.circuit.get_circuit_width()))
-            aux_y = 0
-            
-            #Sobre la superficie anterior dibujamos tantas imagenes como ancho sea el circuito
+            '''#Sobre la superficie anterior dibujamos tantas imagenes como ancho sea el circuito
             for i in range(self.circuit.get_circuit_width()):
                 self.surface.blit(self.image, (0, aux_y))
-                aux_y += self.image.get_height()
+                aux_y += self.image.get_height()'''
             
             #Situamos al coche según el angulo indicado
             
@@ -68,18 +69,20 @@ class Start:
         
         #Si por el contrario la posición es horizontal
         else:
-            #Creamos una superficie con el alto de la carretera del circuito
-            self.surface = pygame.Surface((self.image.get_width() * self.circuit.get_circuit_width(), self.image.get_height()))
-            aux_x = 0
+            #Creamos una superficie con el alto de la carretera del circuito como referencia
+            '''self.surface = pygame.Surface((self.image.get_width() * self.circuit.get_circuit_width(), self.image.get_height()))
+            self.surface.set_alpha(0)
+            aux_x = 0'''
             
-            #Sobre la superficie anterior dibujamos tantas imagenes como ancho sea el circuito
+            '''#Sobre la superficie anterior dibujamos tantas imagenes como ancho sea el circuito
             for i in range(self.circuit.get_circuit_width()):
                 self.surface.blit(self.image, (aux_x, 0))
-                aux_x += self.image.get_width()
+                aux_x += self.image.get_width()'''
+            
+            #Situamos el coche en el angulo adecuado
             
             #Debajo de la linea
             if car_angle == 90:
-                print "Noventaaa"
                 self.game_control.add_player(playercar.PlayerCar(self.game_control, 
                 'cars/coche_prueba_yellow.xml', self.x + self.circuit.get_tile_width() * 2, 
                 self.y - self.circuit.get_tile_height(), 90))
@@ -96,4 +99,20 @@ class Start:
         
         @param screen Superficie destino
         '''
-        screen.blit(self.surface, (self.x - self.game_control.circuit_x(), self.y - self.game_control.circuit_y()))
+        #screen.blit(self.surface, (self.x - self.game_control.circuit_x(), self.y - self.game_control.circuit_y()))
+        
+        #Segun la orientación de la linea de meta, la dibujamos de una forma u otra
+        if self.orientation == 'vertical':
+            aux_y = 0
+            for i in range(self.circuit.get_circuit_width()):
+                screen.blit(self.image, (self.x - self.game_control.circuit_x(), \
+                            aux_y + self.y - self.game_control.circuit_y()))
+                #Obtenemos la posicion de la siguiente
+                aux_y += self.image.get_height()
+        else:
+            aux_x = 0
+            for i in range(self.circuit.get_circuit_width()):
+                screen.blit(self.image, (aux_x + self.x - self.game_control.circuit_x(), \
+                            self. y - self.game_control.circuit_y()))
+                #Obtenemos la posicion de la siguiente
+                aux_x += self.image.get_width()
