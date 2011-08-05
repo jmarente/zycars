819f1647131611e887c0c0c4c0daa95939eb4005
dartmidder <dartmidder@dec2a252-8a97-a28e-c821-f0892bc11fcb>
2011-08-05 09:23:54 +0000

    Completando la pantalla de créditos, con los emails y páginas web

--- engine/mainmenu.py
+++ engine/mainmenu.py
@@ -1,6 +1,7 @@
 #-*- encoding: utf-8 -*-
 
 import data
+import resource
 import keyboard
 import pygame
 import basicmenu
@@ -58,6 +59,7 @@ class MainMenu(basicmenu.BasicMenu):
             
         elif option == "Contrarreloj":
             print "Ha elegido: Contrarreloj"
+            config.Config().set_laps(3)
             config.Config().set_mode(config.TIMED)
             self.game.change_state(charactermenu.CharacterMenu(self.game, 'menu/charactermenu.xml'))
             
@@ -90,16 +92,23 @@ class CreditsMenu(basicmenu.BasicMenu):
             
         #Le pasamos el archivo parseado a BasicMenu para que obtenga los elementos básicos
         self.parser_basic_info(parse)
+        
+        self.tiny_font = resource.get_font('cheesebu', 18)
 
         self.developer = self.font.render('Desarrollador', True, (0,0,0))
         self.developer_name = self.font.render(u'José J. Marente Florín', True, (189, 9, 38))
+        self.developer_email = self.tiny_font.render(u'jose.marente.florin@gmail.com', True, (189, 9, 38))
+        self.developer_web = self.tiny_font.render(u'http://code.google.com/p/zycars/', True, (189, 9, 38))
         self.artist = self.font.render(u'Diseñador gráfico', True, (0,0,0))
         self.artist_name = self.font.render(u'David Nieto Rojas', True, (189, 9, 38))
+        self.artist_email = self.tiny_font.render('info@deividart.com', True, (189, 9, 38))
+        self.artist_web = self.tiny_font.render(u'http://www.deividart.com', True, (189, 9, 38))
         self.music = self.font.render(u'Música', True, (0,0,0))
         self.music_name1 = self.font.render(u'Bob Wizman', True, (189, 9, 38))
         self.music_name2 = self.font.render(u'Pirato Ketchup', True, (189, 9, 38))
         self.music_name3 = self.font.render(u'Los Cadaver', True, (189, 9, 38))
         self.music_name4 = self.font.render(u'The Wavers', True, (189, 9, 38))
+        self.music_name5 = self.font.render(u'Zamalska', True, (189, 9, 38))
 
     def draw(self, screen):
         '''
@@ -111,14 +120,19 @@ class CreditsMenu(basicmenu.BasicMenu):
         self.draw_basic_elements(screen)
         
         screen.blit(self.developer, (20, 200))
-        screen.blit(self.developer_name, (280, 200))
+        screen.blit(self.developer_name, (285, 195))
+        screen.blit(self.developer_email, (285, 220))
+        screen.blit(self.developer_web, (525, 220))
         screen.blit(self.artist, (20, 280))
-        screen.blit(self.artist_name, (280, 280))
+        screen.blit(self.artist_name, (285, 275))
+        screen.blit(self.artist_email, (285, 300))
+        screen.blit(self.artist_web, (525, 300))
         screen.blit(self.music, (20, 360))
         screen.blit(self.music_name1, (280, 360))
         screen.blit(self.music_name2, (280, 390))
         screen.blit(self.music_name3, (280, 420))
         screen.blit(self.music_name4, (280, 450))
+        screen.blit(self.music_name5, (520, 360))
         
         #Dibujamos el cursor
         self.cursor.draw(screen)
